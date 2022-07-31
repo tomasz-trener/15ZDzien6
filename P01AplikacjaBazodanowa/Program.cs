2a42d236eed9817afacce0f2af2dd3c6e0656c79
tomtom <tomasz.les@pw.edu.pl>
2022-07-31 09:48:46 +0200

    Add project files.

---
+++ P01AplikacjaBazodanowa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01AplikacjaBazodanowa
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SqlConnection connection; // do nawiązania polaczenia z bazą
+            SqlCommand command; // przechowuje polecenia SQL
+            SqlDataReader sqlDataReader; // czytanie wyników z tabelki
+
+            string connString = "Data Source=.\\sqlexpress;Initial Catalog=A_Zawodnicy;Integrated Security=True";
+            connection = new SqlConnection(connString);
+
+            command = new SqlCommand("select * from zawodnicy", connection);
+
+            connection.Open();
+
+            sqlDataReader = command.ExecuteReader(); // wysyła polecenie do bazy danych
+
+            //bool czyKoniec= sqlDataReader.Read();
+
+            //string wynik = (string)sqlDataReader.GetValue(2);
+
+            //Console.WriteLine(wynik);
+
+            //sqlDataReader.Read();
+            //wynik = (string)sqlDataReader.GetValue(2);
+            //Console.WriteLine(wynik);
+
+            while (sqlDataReader.Read())
+            {
+                string wynik = (string)sqlDataReader.GetValue(2) + " " + (string)sqlDataReader.GetValue(3);
+                Console.WriteLine(wynik);
+            }
+
+            connection.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
